b8f7e2880c85a8d403df742d5d4434d88b4c376f
Zakarias Abdulkadir <z.k.abdulkadir@gmail.com>
2024-02-09 12:36:11 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    streetAdress: 'k', 
+    name: "zak"
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key] : value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value
+    return obj
+}
+function deleteFromEmployeeByKey(obj, key) {
+    const copy = {
+        ...obj,
+    }
+    delete copy[key]
+    return copy
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+}
